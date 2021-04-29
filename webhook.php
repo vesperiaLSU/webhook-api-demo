f299f950618f338af6e652f402b9c3d5c5bba05e
Chen Guo <guochen1223@gmail.com>
2021-04-29 13:08:27 -0500

    heroku log

--- webhook.php
+++ webhook.php
@@ -2,7 +2,7 @@
 
 $myVar = "hello world!";
 print_r($myVar);
-print_r($_REQUEST);
 
 $input = json_decode(file_get_contents('php://input'), true);
+error_log(print_r($_REQUEST));
 error_log(print_r($input, true));
