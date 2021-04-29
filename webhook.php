ab410566ec59af444f20f101e0ef215bbbcc74e9
Chen Guo <guochen1223@gmail.com>
2021-04-29 12:36:22 -0500

    heroku log

--- webhook.php
+++ webhook.php
@@ -6,3 +6,6 @@ $verify_token = $_REQUEST['hub_verify_token'];
 if ($verify_token == "0b14575d3fb9830de82dd59e0e00465e713d5175") {
     echo $challenge;
 }
+
+$input = json_decode(file_get_contents('php://input'), true);
+error_log(print_r($input, true));
